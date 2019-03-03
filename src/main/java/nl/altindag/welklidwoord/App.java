fae9c9e7f0e86c7a50f675b559ba2ef77acd7af2
Hakan Altindag <hakangoudberg@hotmail.com>
2019-03-03 22:21:37 +0100

    Added loading screen

--- src/main/java/nl/altindag/welklidwoord/App.java
+++ src/main/java/nl/altindag/welklidwoord/App.java
@@ -2,6 +2,7 @@ package nl.altindag.welklidwoord;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
+import java.util.function.Function;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -16,21 +17,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 @SpringBootApplication
 public class App extends Application {
 
     private ConfigurableApplicationContext applicationContext;
     private Parent root;
+    private Stage loadingStage;
     private static final String TITLE = "Welk lidwoord?";
+    private final Function<String, FXMLLoader> fxmlLoaderFunction = fxml -> new FXMLLoader(this.getClass().getResource(fxml));
 
     @Override
     public void init() throws IOException {
+        Platform.runLater(this::displayLoadingScreen);
         applicationContext = new SpringApplicationBuilder(App.class)
                 .headless(false)
                 .run(getParameters().getRaw().stream().toArray(String[]::new));
 
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/mainscreen.fxml"));
+        FXMLLoader fxmlLoader = fxmlLoaderFunction.apply("/mainscreen.fxml");
         fxmlLoader.setControllerFactory(applicationContext::getBean);
         root = fxmlLoader.load();
     }
@@ -43,6 +48,7 @@ public class App extends Application {
         stage.setWidth(800);
         stage.setHeight(600);
         stage.setResizable(false);
+        stage.setOnShowing(windowEvent -> loadingStage.close());
         stage.show();
         FlatterFX.style();
     }
@@ -60,6 +66,26 @@ public class App extends Application {
     @Bean
     public HttpClient httpClient() {
         return HttpClient.newHttpClient();
+    }
+
+    private void displayLoadingScreen() {
+        FXMLLoader fxmlLoader = fxmlLoaderFunction.apply("/loadingscreen.fxml");
+        Parent root;
+
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(root);
+        loadingStage = new Stage();
+        loadingStage.initStyle(StageStyle.UNDECORATED);
+        loadingStage.setResizable(false);
+        loadingStage.setAlwaysOnTop(true);
+        loadingStage.setScene(scene);
+        loadingStage.show();
+        FlatterFX.style();
     }
 
 }
